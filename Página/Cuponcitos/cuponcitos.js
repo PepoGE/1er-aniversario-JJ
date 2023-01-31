31fa9585090e7fda65ed8e2473ead855a3a7e391
José Luis Garza Espinoza <peporinxd@gmail.com>
2023-01-31 15:24:03 -0600

    Añadida las imagenes a las tarjetas

--- Página/Cuponcitos/cuponcitos.js
+++ Página/Cuponcitos/cuponcitos.js
@@ -1,4 +1,13 @@
-var grupoTarjetas = ["J", "E", "S", "S", "I", "C", "A"];
+var foto1 = "../Cuponcitos/Imagenes-Cupones/foto-azulito.jpg";
+var foto2 = "../Cuponcitos/Imagenes-Cupones/foto-cenita.jpg";
+var foto3 = "../Cuponcitos/Imagenes-Cupones/foto-flores.jpg";
+var foto4 = "../Cuponcitos/Imagenes-Cupones/foto-palito.jpg";
+var foto5 = "../Cuponcitos/Imagenes-Cupones/foto-peleita.jpg";
+var foto6 = "../Cuponcitos/Imagenes-Cupones/foto-pinwinos.jpg";
+var foto7 = "../Cuponcitos/Imagenes-Cupones/foto-piojito.jpg";
+var foto8 = "../Cuponcitos/Imagenes-Cupones/foto-pizza.jpg";
+
+var grupoTarjetas = [foto1, foto2, foto3, foto4, foto5, foto6, foto7, foto8];
 
 var totalTarjetas = grupoTarjetas.concat(grupoTarjetas);
 
@@ -13,7 +22,9 @@ function repartirTarjetas() {
     tarjeta.innerHTML =
       '<div id="tarjeta" class="tarjeta">' +
       '<div id="contenidoTarjeta" class="contenidoTarjeta">' +
+      '<img src="' +
       elemento +
+      '" class = "contenidoTarjeta" alt="" srcset=""></img>' +
       "</div>" +
       "</div>";
 
