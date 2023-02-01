b5939519847b8bae3b8ed99ba7d2ce2a565d491f
José Luis Garza Espinoza <peporinxd@gmail.com>
2023-01-31 21:02:11 -0600

    Añadida la función de no abrir más de 2 cartas e identificar entre cartas si son iguales

--- Página/Cuponcitos/cuponcitos.js
+++ Página/Cuponcitos/cuponcitos.js
@@ -11,20 +11,31 @@ var grupoTarjetas = [foto1, foto2, foto3, foto4, foto5, foto6, foto7, foto8];
 
 var totalTarjetas = grupoTarjetas.concat(grupoTarjetas);
 
+function RevolverTarjetas() {
+  var resultado = totalTarjetas.sort(function () {
+    return 0.5 - Math.random();
+  });
+  return resultado;
+}
+
 function repartirTarjetas() {
   var mesa = document.querySelector("#mesa");
+  var tarjetasRevueltas = RevolverTarjetas();
 
   mesa.innerHTML = "";
 
-  totalTarjetas.forEach(function (elemento, index) {
+  tarjetasRevueltas.forEach(function (elemento, index) {
     var tarjeta = document.createElement("div");
 
     tarjeta.innerHTML =
       '<div id="tarjeta" class="tarjeta">' +
       '<img src="' +
       elemento +
-      '" class = "contenidoTarjeta" alt="foto-' +
+      '" class = "contenidoTarjeta" ' +
+      'alt="foto-' +
       index +
+      '" id = "foto-' +
+      elemento +
       '" srcset=""></img>' +
       "</div>";
 
@@ -33,13 +44,31 @@ function repartirTarjetas() {
 }
 
 function descubrir() {
+  var totalDescubiertas = document.querySelectorAll(".descubierta");
+  var descubiertas;
+
+  if (totalDescubiertas.length > 1) {
+    return;
+  }
+
   this.classList.add("descubierta");
+
+  descubiertas = document.querySelectorAll(".descubierta");
+  if (descubiertas.length < 2) {
+    return;
+  }
+
+  var imagenAbierta = document.querySelectorAll(".descubierta img");
+
+  if (imagenAbierta[0].id == imagenAbierta[1].id) {
+    console.log("correcto");
+  } else {
+    console.log("incorrecto");
+  }
 }
 
 repartirTarjetas();
 
-document
-  .querySelectorAll(".tarjeta, .contenidoTarjeta img")
-  .forEach(function (elemento) {
-    elemento.addEventListener("click", descubrir);
-  });
+document.querySelectorAll(".tarjeta").forEach(function (elemento) {
+  elemento.addEventListener("click", descubrir);
+});
