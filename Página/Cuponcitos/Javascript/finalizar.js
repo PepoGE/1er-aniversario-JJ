a11d93711dc2c26ec588a58b6186f1f0932764ac
José Luis Garza Espinoza <peporinxd@gmail.com>
2023-02-17 17:15:36 -0600

    Añadido efecto de sonido al subir de nivel

--- Página/Cuponcitos/Javascript/finalizar.js
+++ Página/Cuponcitos/Javascript/finalizar.js
@@ -2,6 +2,7 @@ function finalizar() {
   if (nivelActual < niveles.length - 1) {
     document.querySelector("#sube-nivel").classList.add("visible");
     document.querySelector("#mesa").classList.remove("visible");
+    document.querySelector("#sonido-subirNivel").play();
   } else {
     document.querySelector("#endGame").classList.add("visible");
     document.querySelector("#mesa").classList.remove("visible");
--- Página/Cuponcitos/Javascript/game-over.js
+++ Página/Cuponcitos/Javascript/game-over.js
@@ -2,4 +2,5 @@ function gameOver() {
   document.querySelector("#gameOver").classList.add("visible");
   document.querySelector("#mesa").classList.remove("visible");
   document.querySelector("#cabecera").style.display = "none";
+  document.querySelector("#sonido-gameOver").play();
 }
