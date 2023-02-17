5789e9eafb7ce32649ba11549063ee3efb6226f5
José Luis Garza Espinoza <peporinxd@gmail.com>
2023-02-17 00:52:37 -0600

    Añadidos movimientos maximos por nivel

--- Página/Cuponcitos/Javascript/variables-glob.js
+++ Página/Cuponcitos/Javascript/variables-glob.js
@@ -7,6 +7,7 @@ var foto6 = "../Cuponcitos/Imagenes-Cupones/foto-pinwinos.jpg";
 var foto7 = "../Cuponcitos/Imagenes-Cupones/foto-piojito.jpg";
 var foto8 = "../Cuponcitos/Imagenes-Cupones/foto-pizza.jpeg";
 
+var movimientos = 0;
 var grupoTarjetas = [
   [foto1, foto2],
   [foto3, foto4],
@@ -17,12 +18,15 @@ var nivelActual = 0;
 var niveles = [
   {
     tarjetas: grupoTarjetas[0],
+    movimientosMax: 3
   },
   {
     tarjetas: grupoTarjetas[0].concat(grupoTarjetas[1]),
+    movimientosMax: 8
   },
   {
     tarjetas: grupoTarjetas[0].concat(grupoTarjetas[1], grupoTarjetas[2]),
+    movimientosMax: 12
   },
   {
     tarjetas: grupoTarjetas[0].concat(
@@ -30,5 +34,6 @@ var niveles = [
       grupoTarjetas[2],
       grupoTarjetas[3]
     ),
+    movimientosMax: 20
   },
 ];
